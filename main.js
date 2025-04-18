86be156caeaa9e7619f80fdb5dc1b7c72e88cbb6
jdh07310 <wjdehdgur07@naver.com>
2025-04-18 18:24:52 +0900

    1st

---
+++ main.js
@@ -0,0 +1,32 @@
+function toggleContent() {
+    var content = document.querySelector('.toggle-content');
+    // 내용을 펼치거나 숨기는 함수
+    content.style.display = (content.style.display === 'none' || content.style.display === '') ? 'block' : 'none';
+  }
+  function toggleHistoryContent() {
+    var history = document.querySelector('.toggle-history');
+    // 내용을 펼치거나 숨기는 함수
+    history.style.display = (history.style.display === 'none' || history.style.display === '') ? 'block' : 'none';
+  }
+  function toggleapplicationsContent() {
+    var applications = document.querySelector('.toggle-applications');
+    // 내용을 펼치거나 숨기는 함수
+    applications.style.display = (applications.style.display === 'none' || applications.style.display === '') ? 'block' : 'none';
+  }
+  function toggleprosconsContent() {
+    var proscons = document.querySelector('.toggle-pros-cons');
+    // 내용을 펼치거나 숨기는 함수
+    proscons.style.display = (proscons.style.display === 'none' || proscons.style.display === '') ? 'block' : 'none';
+  }
+  function togglefutureContent() {
+    var future = document.querySelector('.toggle-future');
+    // 내용을 펼치거나 숨기는 함수
+    future.style.display = (future.style.display === 'none' || future.style.display === '') ? 'block' : 'none';
+  }
+  function togglereferencesContent() {
+    var references = document.querySelector('.toggle-references');
+    // 내용을 펼치거나 숨기는 함수
+    references.style.display = (references.style.display === 'none' || references.style.display === '') ? 'block' : 'none';
+  }
+  
+  
